c99ebc2358337783f07ff1f75c0fa47aa09ea388
Lance Valle <vallelance8@gmail.com>
2025-02-14 19:31:25 +0800

    feat: add chapter 3 notes

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-console.log("Hello world")
+console.log(0xBADCAFE)
+console.log(0xff + 0o377)
+console.log(0xffddf47)
 
-const plus1 = x => 1 + 2
+//floating numbers
+console.log(1_000_000_000)
+console.log(0x89_AB_CD_EF)
+console.log(0b0001_1101_0111)
+console.log(0.123_456_789)
+
+console.log(Math.pow(53,3))
+console.log(53*53*53)
+console.log(Math.round(5.01))
+console.log(Math.ceil(5.01))
+console.log(Math.floor(5.99))
+console.log(Math.abs(-99))
+console.log(Math.max(100,99,200,33,1,-99))
+console.log(Math.min(100,99,200,33,1,-99))
+console.log(Math.random(5,5))
+console.log(Math.PI)
+console.log(Math.E)
+console.log(Math.sqrt(9))
+console.log(Math.pow(3,3/3))
+console.log(Math.sin(0))
+console.log(Math.log(10))
 
 
-// unequal, less than, greater than, and so on. They evaluate to true or false.
-let x = 2, y = 3;          
-// These = signs are assignment, not equality tests
- x === y                    
-x !== y                    
-x < y                      
-x <= y                     
-x > y                      
-x >= y                     
-"two" === "three"          
-"two" > "three"            
-false === (x > y)          
-// => false: equality
- // => true: inequality
- // => true: less-than
- // => true: less-than or equal
- // => false: greater-than
- // => false: greater-than or equal
- // => false: the two strings are different
- // => true: "tw" is alphabetically greater than "th"
- // => true: false is equal to false
+let bigNumber = 1000;
+console.log(Number.isFinite(bigNumber))
